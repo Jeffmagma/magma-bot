2c652e953b6b712de0e2c5b0b5d20b94e581290f
Jeffmagma <jeffrey.magmachu@gmail.com>
2017-11-06 19:58:52 -0500

    fixed events

--- src/main/java/MagmaBot.java
+++ src/main/java/MagmaBot.java
@@ -1,28 +1,31 @@
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.events.Event;
 import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.api.events.IListener;
+import sx.blah.discord.handle.impl.events.ReadyEvent;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.MessageBuilder;
 
-public class MagmaBot implements IListener<MessageReceivedEvent> {
+public class MagmaBot implements IListener<Event> {
     private IDiscordClient client;
 
     MagmaBot(IDiscordClient client) {
         this.client = client;
-        EventDispatcher dispatcher = client.getDispatcher(); // Gets the client's event dispatcher
-        dispatcher.registerListener(this); // Registers this bot as an event listener
-        client.changePlayingText("!help for help");
     }
 
     private void sendMessage(String message, MessageReceivedEvent event) {
-        new MessageBuilder(this.client).withChannel(event.getChannel()).withContent(message).send();
+        new MessageBuilder(this.client).withChannel(event.getChannel()).withContent(message).build();
     }
 
-
-    public void handle(MessageReceivedEvent event) {
-        IMessage message = event.getMessage();
-        if (message.getContent().equalsIgnoreCase("!help"))
-            sendMessage(message.getAuthor().mention() + " asked for help", event);
+    public void handle(Event event) {
+        if (event instanceof MessageReceivedEvent) {
+            MessageReceivedEvent messageReceivedEvent = ((MessageReceivedEvent)event);
+            IMessage message = messageReceivedEvent.getMessage();
+            if (message.getContent().equalsIgnoreCase("!help"))
+                sendMessage(message.getAuthor().mention() + " asked for help", messageReceivedEvent);
+        } else if (event instanceof ReadyEvent) {
+            client.changePlayingText("!help for help");
+        }
     }
 }
